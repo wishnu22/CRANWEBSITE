5647f7ba2ad6a67bb82e081dd0d2aee463c078ff
wishnu22 <vishnuunnikrishnan584@gmail.com>
2024-11-27 22:27:12 +0530

    main'

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,12 +7,12 @@ const Hero = () => {
       <div className="fireball-container">
         <div className="fireball fireball-1">Health</div>
         <div className="fireball fireball-2">Beauty</div>
-        <div className="fireball fireball-3">Technology</div>
+        <div className="fireball fireball-3">Techn</div>
         <div className="fireball fireball-4 ">Style</div>
        {/* <div className="fireball fireball-5">Technology</div> */}
       </div>
-      <h1>Welcome to Cranvalor Innovations</h1>
-      <p><strong>Your gateway to a healthier, more beautiful, and tech-driven world.</strong></p>
+      <h1 className="hero-heading">Welcome to <span className="highlight">Cranvalor Innovations</span></h1>
+      <p className="hero-subheading"><strong>Your gateway to a healthier, more beautiful, and tech-driven world.</strong></p>
       <a href="#products" className="explore-btn">Explore</a>
     </section>
   );

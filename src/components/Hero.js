78267671aeaf9f376ddf84f83a7b458fd715f7e4
wishnu22 <vishnuunnikrishnan584@gmail.com>
2024-11-29 12:35:52 +0530

    'main

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="fireball-container">
-        <div className="fireball fireball-1">Health</div>
+       <div className="fireball fireball-1">Health</div>
         <div className="fireball fireball-2">Beauty</div>
-        <div className="fireball fireball-3">Tech</div>
-        <div className="fireball fireball-4">Style</div>
+        <div className="fireball fireball-3">Style</div>
+        <div className="fireball fireball-4">Tech</div>
       </div>
       <h1 className="hero-heading">Welcome to <span className="highlight">Cranvalor Innovations</span></h1>
       <p className="hero-subheading"><strong>Your gateway to a healthier, more beautiful, and tech-driven world.</strong></p>
